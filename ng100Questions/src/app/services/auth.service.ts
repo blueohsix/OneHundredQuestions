c1145e3f797f067589302a5beddcc80b0989f062
Casey Asher <casey.e.asher@outlook.com>
2019-11-23 13:08:34 -0600

    untested services

--- ng100Questions/src/app/services/auth.service.ts
+++ ng100Questions/src/app/services/auth.service.ts
@@ -1,9 +1,76 @@
 import { Injectable } from '@angular/core';
+import { tap, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private baseUrl = 'http://localhost:8050/'; // development
+  // private baseUrl = '/OneHundredQuestions/'; // production
+  logoutSuccess: boolean;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(username, password) {
+    // Make credentials
+    const credentials = this.generateBasicAuthCredentials(username, password);
+    // Send credentials as Authorization header (this is spring security convention for basic auth)
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: `Basic ${credentials}`,
+        'X-Requested-With': 'XMLHttpRequest'
+      })
+    };
+
+    // create request to authenticate credentials
+    return this.http
+      .get(this.baseUrl + 'authenticate', httpOptions)
+      .pipe(
+        tap((res) => {
+          localStorage.setItem('credentials' , credentials);
+          return res;
+        }),
+        catchError((err: any) => {
+          console.log(err);
+          return throwError('AuthService.login(): Error logging in.');
+        })
+        );
+  }
+
+  register(user) {
+    // create request to register a new account
+    return this.http.post(this.baseUrl + 'register', user)
+    .pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError('AuthService.register(): error registering user.');
+      })
+    );
+  }
+
+  logout() {
+    try {
+      localStorage.removeItem('credentials');
+      this.logoutSuccess = true;
+    } catch (error) {
+      console.error('logout failure');
+    }
+  }
+
+  checkLogin() {
+    if (localStorage.getItem('credentials')) {
+      return true;
+    }
+    return false;
+  }
+
+  generateBasicAuthCredentials(username, password) {
+    return btoa(`${username}:${password}`);
+  }
+
+  getCredentials() {
+    return localStorage.getItem('credentials');
+  }
 }
