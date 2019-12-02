79968b390528c607be0608e13fadf0f3199e0d5b
Casey Asher <casey.e.asher@outlook.com>
2019-12-02 00:12:23 -0600

    edit account foundation laid. Needs to be tied into userService

--- ng100Questions/src/app/components/profile/profile.component.ts
+++ ng100Questions/src/app/components/profile/profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/user';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +10,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  currentUser: User;
+  editPorFavor: boolean;
 
-  constructor() { }
+  constructor(private auth: AuthService, private user: UserService) {}
 
   ngOnInit() {
+    if (this.checkLogin()) {
+      this.getProfile();
+    }
   }
+  checkLogin() {
+    if (!this.auth.checkLogin()) {
+      this.currentUser = null;
+      this.currentUser = new User();
+    }
+    return this.auth.checkLogin();
+  }
+  getProfile() {
+    this.user.showByUsername(this.auth.getUsername()).subscribe(
+      lifeIsGood => {
+        this.currentUser = lifeIsGood;
+      },
+      whenThingsGoBad => {
+        console.error('error in getProfile()');
+      }
+    );
+  }
+  editAccount(form: NgForm) {
+    console.log(form.value);
+    console.log(this.user);
 
+  }
+  toggle() {
+    this.editPorFavor = !this.editPorFavor;
+  }
 }
